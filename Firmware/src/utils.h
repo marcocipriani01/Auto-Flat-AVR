5ca3e9884ce4ca093d04089b40d82a4fbaa9df24
marcocipriani01 <marco.cipriani.01@gmail.com>
2023-05-19 18:34:46 +0200

    Stole some macros from Arduino

---
+++ Firmware/src/utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define constrain(amt, low, high) ((amt) < (low) ? (low) : ((amt) > (high) ? (high) : (amt)))
+
+#endif
