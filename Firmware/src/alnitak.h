ecbdb051c1051dbdfc79c92cfb1a2f0a94155d0c
marcocipriani01 <marco.cipriani.01@gmail.com>
2023-05-19 18:34:14 +0200

    Altak protocol definitions

---
+++ Firmware/src/alnitak.h
@@ -0,0 +1,37 @@
+#ifndef ALNITAK_H
+#define ALNITAK_H
+
+#include "config.h"
+
+// Alnitak device IDs
+#define FLAT_MAN_L 10
+#define FLAT_MAN_XL 15
+#define FLAT_MAN 19
+#define FLIP_FLAT 99
+
+#ifdef SERVO_PIN
+#define DEVICE_ID FLIP_FLAT
+
+typedef enum {
+    NEITHER_OPEN_NOR_CLOSED = 0,
+    CLOSED,
+    OPEN,
+    TIMED_OUT
+} ShutterStatus;
+
+typedef enum {
+    OPENING = 0,
+    CLOSING,
+    NONE
+} MotorDirection;
+
+typedef enum {
+    STOPPED = 0,
+    RUNNING
+} MotorStatus;
+
+#else
+#define DEVICE_ID FLAT_MAN
+#endif
+
+#endif
