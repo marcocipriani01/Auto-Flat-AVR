2598e6df702e598abe30b52f192e777954bdfc86
marcocipriani01 <marco.cipriani.01@gmail.com>
2023-05-29 18:55:10 +0200

    Cleanup servo code

--- Firmware/src/servo.c
+++ Firmware/src/servo.c
@@ -1,33 +1,17 @@
 #include "servo.h"
 
+volatile bool servoHigh;
 volatile uint16_t servoTicks;
-volatile uint8_t servoHigh;
-
-uint8_t calcPrescalerBits(uint16_t prescaler) {
-    switch (prescaler) {
-        case 1:
-            return _BV(CS10);
-        case 8:
-            return _BV(CS11);
-        case 64:
-            return _BV(CS11) | _BV(CS10);
-        case 256:
-            return _BV(CS12);
-        case 1024:
-            return _BV(CS12) | _BV(CS10);
-    }
-    return 0;
-}
 
 void initServo(uint16_t pulseWidth) {
-    servoHigh = 0;
-    TCCR1A = 0;                                   // Normal counting mode
-    TCCR1B = calcPrescalerBits(SERVO_PRESCALER);  // Prescaler
-    TCNT1 = 0;                                    // Clear timer count
-    TIFR1 |= _BV(OCF1A);                          // Clear pending interrupts
-    OCR1A = 0xFFFF;                               // Set the Output Compare Register to the maximum value
-    TIMSK1 |= _BV(OCIE1A);                        // Enable the output compare interrupt
-    DDRD |= _BV(PD5);                             // Set PD5 as output
+    servoHigh = false;
+    TCCR1A = 0;                        // Normal counting mode
+    TCCR1B = TIMER1_CLOCK_SOURCE;      // Prescaler 8
+    TCNT1 = 0;                         // Clear timer count
+    TIFR1 |= _BV(OCF1A);               // Clear pending interrupts
+    OCR1A = 0xFFFF;                    // Set the Output Compare Register to the maximum value
+    TIMSK1 |= _BV(OCIE1A);             // Enable the output compare interrupt
+    DDRD |= _BV(PD5);                  // Set PD5 as output
     setServoPulseWidth(pulseWidth);
 }
 
@@ -36,15 +20,15 @@ void setServoPulseWidth(uint16_t pulseWidth) {
 }
 
 ISR(TIMER1_COMPA_vect) {
-    if (servoHigh == 0) {
-        PORTD |= _BV(PD5);
-        servoHigh = 1;
-        OCR1A = servoTicks;
+    if (servoHigh) {
+        PORTD &= ~_BV(PD5);
+        servoHigh = false;
+        OCR1A = 0xFFFF;
         TCNT1 = 0;
     } else {
-        PORTD &= ~_BV(PD5);
-        servoHigh = 0;
-        OCR1A = 0xFFFF;
+        PORTD |= _BV(PD5);
+        servoHigh = true;
+        OCR1A = servoTicks;
         TCNT1 = 0;
     }
 }
--- Firmware/src/servo.h
+++ Firmware/src/servo.h
@@ -1,6 +1,7 @@
 #ifndef SERVO_H
 #define SERVO_H
 
+#include <stdbool.h>
 #include <inttypes.h>
 #include <avr/interrupt.h>
 
@@ -8,14 +9,25 @@
 #if SERVO_PRESCALER != 1 && SERVO_PRESCALER != 8 && SERVO_PRESCALER != 64 && SERVO_PRESCALER != 256 && SERVO_PRESCALER != 1024
 #error "Invalid prescaler value"
 #endif
+#if SERVO_PRESCALER == 1
+#define TIMER1_CLOCK_SOURCE _BV(CS10)
+#elif SERVO_PRESCALER == 8
+#define TIMER1_CLOCK_SOURCE _BV(CS11)
+#elif SERVO_PRESCALER == 64
+#define TIMER1_CLOCK_SOURCE (_BV(CS11) | _BV(CS10))
+#elif SERVO_PRESCALER == 256
+#define TIMER1_CLOCK_SOURCE _BV(CS12)
+#elif SERVO_PRESCALER == 1024
+#define TIMER1_CLOCK_SOURCE _BV(CS12) | _BV(CS10)
+#endif
+
 #define CLOCKS_PER_us (F_CPU / 1000000L)
 #define us_TO_TICKS(us) ((CLOCKS_PER_us * us) / SERVO_PRESCALER)
 #define TICKS_TO_us(ticks) ((ticks * SERVO_PRESCALER) / CLOCKS_PER_us)
 
+extern volatile bool servoHigh;
 extern volatile uint16_t servoTicks;
-extern volatile uint8_t servoHigh;
 
-uint8_t calcPrescalerBits(uint16_t prescaler);
 void initServo(uint16_t pulseWidth);
 void setServoPulseWidth(uint16_t pulseWidth);
 
