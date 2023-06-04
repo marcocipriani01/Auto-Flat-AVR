1a7f4380945405e050c5b7cb7d78c6de11f824ba
marcocipriani01 <marco.cipriani.01@gmail.com>
2023-06-04 19:32:16 +0200

    Fix nasty int size bug

--- Firmware/src/utils.c
+++ Firmware/src/utils.c
@@ -1,17 +1,20 @@
 #include "utils.h"
 
-int abs(int x) {
-    return (x < 0) ? (-x) : x;
-}
-
-int constrain(int x, int low, int high) {
-    return (x < low) ? low : ((x > high) ? high : x);
-}
-
-int linearMap(int x, int in_min, int in_max, int out_min, int out_max) {
-    int out = ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
-    if (out_min < out_max)
-        return constrain(out, out_min, out_max);
+int32_t constrain(int32_t x, int32_t low, int32_t high) {
+    if (x < low)
+        return low;
+    else if (x > high)
+        return high;
     else
-        return constrain(out, out_max, out_min);
+        return x;
+}
+
+int32_t linearMap(int32_t x, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max) {
+    int32_t out = ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+    if (out < out_min)
+        return out_min;
+    else if (out > out_max)
+        return out_max;
+    else
+        return out;
 }
--- Firmware/src/utils.h
+++ Firmware/src/utils.h
@@ -1,11 +1,9 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#define min(a, b) ((a) < (b) ? (a) : (b))
-#define max(a, b) ((a) > (b) ? (a) : (b))
+#include <inttypes.h>
 
-int abs(int x);
-int constrain(int x, int low, int high);
-int linearMap(int x, int in_min, int in_max, int out_min, int out_max);
+int32_t constrain(int32_t x, int32_t low, int32_t high);
+int32_t linearMap(int32_t x, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max);
 
 #endif
